3e63a8cfa3e3997f234a14020653b1288d797a0a
Anusha Talagapu <anushatalagapu@gmail.com>
2024-05-07 20:22:00 +0530

    Updated sorting algo

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,14 +1,20 @@
 #include<iostream>
 using namespace std;
 void bubble_sort(int arr[], int n) {
+    int didswap=0;
     for (int i = n - 1; i >= 0; i--) { 
         for (int j = 0; j < i; j++) { 
             if (arr[j] > arr[j + 1]) {
                 int temp = arr[j + 1];
                 arr[j + 1] = arr[j];
                 arr[j] = temp;
+                didswap=1;
             }
         }
+    
+    if(didswap==0){
+        break;
+    }
     }
 }
 
